904abaebde5af27a767801879b860c875890666b
Alex M. Podobas <ampodobas@cloudcompli.com>
2017-03-14 00:59:46 -0700

    Update is_two_factor_enabled.sh

--- is_two_factor_enabled.sh
+++ is_two_factor_enabled.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
+### Instructions: replace YOUR_IPV4_TO_TEST with an IP address you wish to test
+
 ###############################
 ### Define IPs to check
 ###############################
 
-#Each IPv4 within double quotes will be checked for two-factor authentication. Delimiter is whitespace. 
+### Each IPv4 within double quotes will be checked for two-factor authentication. Delimiter is whitespace. 
 #TBD: declare -a arrayHosts=("some host" "another host");
 
 ###############################
 ### Step 1
 ###############################
 
-# What this does: removes the temporary files for the output of the script command
-rm -rf /var/log/temp_2FAEnabled_*.log
+### What this does: remove temporary file:
+rm -rf /tmp/kill_is_two_factor_enabled_1
 
 ###############################
 ### Step 2
 ###############################
 
-# What this does: invoke the script (sepatate file) used to to kill this script, which is necessary to exit from the input prompt (e.g., "password:" or "Verification code:") because this script operates as a non-interactive shell
-sh ./kill_is_two_factor_enabled.sh &
+### What this does: invoke the script (separate file) used to to kill this script, which is necessary to exit from the input prompt (e.g., "password:" or "Verification code:") because this script operates as a non-interactive shell
+sh ./kill_is_two_factor_enabled_1.sh &
 
 ###############################
 ### Step 3
 ###############################
-# What this does: save the PID of this script ($$) and the kill command for it. Note that -9 can be replaced by any signal
-echo "kill -9" $$ > /tmp/kill_is_two_factor_enabled
+### What this does: save the PID of this script ($$) and the kill command for it. Note that -9 can be replaced by any signal.
+### Arguments used in bash command:
+  	# -a: append the output to file or typescript, retaining the prior contents.
+  
+echo "kill -9" $$ > /tmp/kill_is_two_factor_enabled_1
 
 ###############################
 ### Step 4
@@ -35,8 +40,12 @@ echo "kill -9" $$ > /tmp/kill_is_two_factor_enabled
 #do
    
    #What this does: use script command to pass command argument to write output of SSH attempt, then remove certain undesired formatting:
-   script -c  'ssh -T root@SOMEHOST' /var/log/is_two_factor_enabled.log;
+   
+   script -c "echo 'for YOUR_IPV4_TO_TEST'; ssh root@YOUR_IPV4_TO_TEST" -a /var/log/is_two_factor_enabled.log -f
 
 #done 
 
 ### Note: Steps 5 and 6 are in "kill_is_two_factor_enabled" script. /var/log/is_two_factor_enabled.log is collected by Filebeat and sent to the ELK stack.
+
+
+
